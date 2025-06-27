e7943de28c49e711196b4e7576f01b026d4b321c
Richard-Sebastian-C <richardchurchil2010@gmail.com>
2025-06-26 19:22:56 -0500

    Fixed Bugs and added comments. NEXT STEP: change the code based of the comments

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -1,5 +1,7 @@
 package pedroPathing.constants;
 
+import static com.pedropathing.localization.Localizers.THREE_WHEEL;
+
 import com.pedropathing.localization.Localizers;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.util.CustomFilteredPIDFCoefficients;
@@ -10,23 +12,23 @@ public class FConstants {
     static {
         FollowerConstants.localizers = Localizers.THREE_WHEEL;
 
-        FollowerConstants.leftFrontMotorName = "leftFront";
-        FollowerConstants.leftRearMotorName = "leftRear";
-        FollowerConstants.rightFrontMotorName = "rightFront";
-        FollowerConstants.rightRearMotorName = "rightRear";
+        FollowerConstants.leftFrontMotorName = "FL";
+        FollowerConstants.leftRearMotorName = "BL";
+        FollowerConstants.rightFrontMotorName = "FR";
+        FollowerConstants.rightRearMotorName = "BR";
 
-        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
-        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
-        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
-        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.FORWARD;    //"FL"
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.FORWARD;     //"BL"
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.REVERSE;   //"FR"
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.REVERSE;    //"BR"
 
-        FollowerConstants.mass = 13;
+        FollowerConstants.mass = 12.07; // the mass needs to be in kilos
 
-        FollowerConstants.xMovement = 57.8741;
-        FollowerConstants.yMovement = 52.295;
+        FollowerConstants.xMovement = 670.9776;
+        FollowerConstants.yMovement = 527.833;
 
-        FollowerConstants.forwardZeroPowerAcceleration = -41.278;
-        FollowerConstants.lateralZeroPowerAcceleration = -59.7819;
+        FollowerConstants.forwardZeroPowerAcceleration = -3190.8914;
+        FollowerConstants.lateralZeroPowerAcceleration = -1141.3884;
 
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
         FollowerConstants.useSecondaryTranslationalPID = false;
--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -5,18 +5,18 @@ import com.pedropathing.localization.constants.*;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
-        ThreeWheelConstants.strafeTicksToInches = .001989436789;
-        ThreeWheelConstants.turnTicksToInches = .001989436789;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
-        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        ThreeWheelConstants.forwardTicksToInches = 0.2926;
+        ThreeWheelConstants.strafeTicksToInches = -0.0217;
+        ThreeWheelConstants.turnTicksToInches = -0.0005;
+        ThreeWheelConstants.leftY = 1; //inches from center of robot
+        ThreeWheelConstants.rightY = -1; //inches from center of robot
+        ThreeWheelConstants.strafeX = -2.5; //inches from center of robot
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "FL";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "BR";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "FR";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.FORWARD; //FL
+        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE; //BR
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD; //FR
     }
 }
 
